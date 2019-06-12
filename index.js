7003bd4a7a2c4bb054a65c0379dcb084128dcd16
Agustina Sosa <agustinasf85@gmail.com>
2019-06-11 21:29:28 -0400

    Added express. - port:4400

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // implement your API here
+const express = require('express');
+const db = require('./data/db');
+
+const server = express();
+
+
+server.listen(4400, () => console.log('My first express server is running on port 4400'));
