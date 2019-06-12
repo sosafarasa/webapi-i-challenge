26581d604a0d8c278f5a32f80875031846f9e86f
Agustina Sosa <agustinasf85@gmail.com>
2019-06-11 21:58:05 -0400

    Added post endpoint

--- index.js
+++ index.js
@@ -4,5 +4,19 @@ const db = require('./data/db');
 
 const server = express();
 
+server.use(express.json());
 
-server.listen(4400, () => console.log('My first express server is running on port 4400'));
+
+server.post('/api/users', (req, res) => {
+    const info = req.body;
+    if(!info.name || ! info.bio){
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." })
+    } else {
+        db.insert(info)
+        .then(user => res.status(201).json(user))
+        .catch(err => res.status(500).json({ error: "There was an error while saving the user to the database" }))
+    }
+})
+
+
+server.listen(4400, () => console.log('Listening on port 4400'));
