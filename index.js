df31f9015afabb7c181f2c2da6ed8d528db75d2a
Agustina Sosa <agustinasf85@gmail.com>
2019-06-11 22:19:56 -0400

    Added find endpoint

--- index.js
+++ index.js
@@ -21,10 +21,16 @@ server.post('/api/users', (req, res) => {
     }
 })
 
+server.get('/api/users', (req, res) => {
+    db.find()
+    .then(users => res.status(200).json(users))
+    .catch(err => res.status(404).json({ error: "The users information could not be retrieved." }))
+})
+
 server.get('/api/users/:id', (req, res) => {
     db.findById(id)
     .then(users => res.status(200).json(users))
-    .catch(err => res.status(404).json({ error: "The users information could not be retrieved." }))
+    .catch(err => res.status(404).json({ message: "The user with the specified ID does not exist." }))
 })
 
 
